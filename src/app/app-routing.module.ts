54910bcfae3b8c4f49c9baa968b092e9a79ceb35
Arthur Chang <arthurc0102@gmail.com>
2019-02-25 16:12:13 +0800

    Use hash at url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ExtraOptions, Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
+const config: ExtraOptions = {
+  useHash: true,
+};
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, config)],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
